487d7e4e99c8ac1b9136683fe8d245063ec63f54
ssamueluk <ssamueluk@example.com>
2023-05-19 17:23:31 +0100

    cli path update

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -1,13 +1,13 @@
-# ## go to tmp directory
-# cd /tmp
+## go to tmp directory
+cd /tmp
 
-# ## Ensure AWS CLI v2 is installed
-# sudo yum -y remove aws-cli
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip -q awscliv2.zip
-# sudo ./aws/install
-# rm awscliv2.zip
-# export PATH=~/.local/bin:$PATH
+## Ensure AWS CLI v2 is installed
+sudo yum -y remove aws-cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+export PATH=~/.local/bin:$PATH
 
 # Install newer version of AWS SAM CLI
 wget -q https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
@@ -16,7 +16,7 @@ sudo ./sam-installation/install --update
 rm -rf ./sam-installation/
 rm ./aws-sam-cli-linux-x86_64.zip
 
-## Install additional dependencies
-sudo yum install -y jq
-npm install -g aws-cdk --force
+# ## Install additional dependencies
+# sudo yum install -y jq
+# npm install -g aws-cdk --force
 
