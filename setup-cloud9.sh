89287c6f59864b7684a8cb0f519a7c9c73774bb1
ssamueluk <7886971+ssamueluk@users.noreply.github.com>
2023-05-22 16:05:19 +0100

    Update setup-cloud9.sh

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -16,6 +16,25 @@ sudo ./sam-installation/install --update
 rm -rf ./sam-installation/
 rm ./aws-sam-cli-linux-x86_64.zip
 
+
+# Install Python 3.10
+# Update sudo config to enable sudo check in homebrew
+# https://github.com/Homebrew/install/issues/369
+# https://askubuntu.com/questions/1195249/sudo-validate-sudo-v-asks-for-password-even-with-nopasswd/1211226
+echo "Defaults verifypw = any" | sudo tee /etc/sudoers.d/100-linuxbrew-install
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Configure Homebrew as per instructions printed from install
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/ec2-user/.bash_profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# Install our dependencies (slow on a t2.micro)
+brew install python@3.10 jq awscli aws-sam-cli 
+# Force python 3.10 to top of PATH
+echo 'PATH=/home/linuxbrew/.linuxbrew/opt/python@3.10/libexec/bin:$PATH' >> ~/.bash_profile
+. ~/.bash_profile
+# Install poetry
+curl -sSL https://install.python-poetry.org | python3.10 -
+
 ## Install additional dependencies
 sudo yum install -y jq
 
